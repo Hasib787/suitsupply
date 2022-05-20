eddc0928e45812fb9e2b398901ad905e5dfd6857
Md. Masud Rana <mr.hasib45@gmail.com>
2022-05-20 23:37:40 +0600

    header done

--- src/components/Shared/Header/Navbar/Navbar.js
+++ src/components/Shared/Header/Navbar/Navbar.js
@@ -1,11 +1,90 @@
-import React from 'react';
-
+import { React, useCallback, useEffect, useRef, useState } from "react";
+import "./Navbar.style.css";
+import logo from "../../../../assets/logos/suitsupply-logo.svg";
 const Navbar = () => {
-    return (
+  let navbarRef = useRef(null);
+
+  const [position, setPosition] = useState(window.scrollY);
+  const [navbarHide, setNavbarHide] = useState(true);
+
+  const handleNavigation = useCallback(
+    (e) => {
+      const window = e.currentTarget;
+      if (position > window.scrollY) {
+        setNavbarHide(true);
+        console.log("scrolling up");
+        navbarRef.current.className = "header_nav";
+      } else if (position < window.scrollY) {
+        setNavbarHide(false);
+        console.log("scrolling down");
+        navbarRef.current.className = "navbar_hidden header_nav";
+      }
+      setPosition(window.scrollY);
+    },
+    [position]
+  );
+
+  useEffect(() => {
+    setPosition(window.scrollY);
+    window.addEventListener("scroll", handleNavigation);
+
+    return () => {
+      window.removeEventListener("scroll", handleNavigation);
+    };
+  }, [handleNavigation]);
+
+  return (
+    <>
+      <div ref={navbarRef} className="header_nav ">
         <div>
-            
+          <i
+            style={{ fontSize: 22, cursor: "pointer" }}
+            className="fas fa-bars"
+          ></i>
         </div>
-    );
+        <div className="headerLogo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="d-flex ">
+          <a
+            className="nav_link"
+            href="https://suitsupply.com/en-bd/login?rurl=1"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span
+              className="me-3"
+            >
+              <i class="far fa-user"></i>
+            </span>
+          </a>
+          <a
+            className="nav_link"
+            href="https://suitsupply.com/en-bd/wishlist"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span>
+              <i class="far fa-bookmark"></i>
+            </span>
+          </a>
+          <a
+            className="nav_link"
+            href="https://suitsupply.com/en-bd/cart"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span
+              className="ms-3"
+            >
+              <i class="fas fa-shopping-bag"></i>
+            </span>
+          </a>
+        </div>
+      </div>
+      {navbarHide && <div style={{ height: "55px" }}></div>}
+    </>
+  );
 };
 
 export default Navbar;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React from "react";
+import Header from "../../components/Shared/Header/Header";
 
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <h1>Home</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe perferendis quis architecto suscipit ipsam, nostrum reiciendis veniam alias amet, sed aliquam a delectus provident odio incidunt quos dignissimos, dolorem aspernatur.
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit aspernatur corporis voluptatum. Animi ducimus earum error distinctio molestiae quisquam optio voluptatibus illo pariatur et quasi, quidem natus, sint unde inventore!
+      </p>
+      <h1>Home</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe perferendis quis architecto suscipit ipsam, nostrum reiciendis veniam alias amet, sed aliquam a delectus provident odio incidunt quos dignissimos, dolorem aspernatur.
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit aspernatur corporis voluptatum. Animi ducimus earum error distinctio molestiae quisquam optio voluptatibus illo pariatur et quasi, quidem natus, sint unde inventore!
+      </p>
+      <h1>Home</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe perferendis quis architecto suscipit ipsam, nostrum reiciendis veniam alias amet, sed aliquam a delectus provident odio incidunt quos dignissimos, dolorem aspernatur.
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit aspernatur corporis voluptatum. Animi ducimus earum error distinctio molestiae quisquam optio voluptatibus illo pariatur et quasi, quidem natus, sint unde inventore!
+      </p><h1>Home</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe perferendis quis architecto suscipit ipsam, nostrum reiciendis veniam alias amet, sed aliquam a delectus provident odio incidunt quos dignissimos, dolorem aspernatur.
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit aspernatur corporis voluptatum. Animi ducimus earum error distinctio molestiae quisquam optio voluptatibus illo pariatur et quasi, quidem natus, sint unde inventore!
+      </p>
+      <h1>Home</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe perferendis quis architecto suscipit ipsam, nostrum reiciendis veniam alias amet, sed aliquam a delectus provident odio incidunt quos dignissimos, dolorem aspernatur.
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit aspernatur corporis voluptatum. Animi ducimus earum error distinctio molestiae quisquam optio voluptatibus illo pariatur et quasi, quidem natus, sint unde inventore!
+      </p>
+      <h1>Home</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe perferendis quis architecto suscipit ipsam, nostrum reiciendis veniam alias amet, sed aliquam a delectus provident odio incidunt quos dignissimos, dolorem aspernatur.
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit aspernatur corporis voluptatum. Animi ducimus earum error distinctio molestiae quisquam optio voluptatibus illo pariatur et quasi, quidem natus, sint unde inventore!
+      </p>
+      <h1>Home</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe perferendis quis architecto suscipit ipsam, nostrum reiciendis veniam alias amet, sed aliquam a delectus provident odio incidunt quos dignissimos, dolorem aspernatur.
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit aspernatur corporis voluptatum. Animi ducimus earum error distinctio molestiae quisquam optio voluptatibus illo pariatur et quasi, quidem natus, sint unde inventore!
+      </p>
+      <h1>Home</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe perferendis quis architecto suscipit ipsam, nostrum reiciendis veniam alias amet, sed aliquam a delectus provident odio incidunt quos dignissimos, dolorem aspernatur.
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit aspernatur corporis voluptatum. Animi ducimus earum error distinctio molestiae quisquam optio voluptatibus illo pariatur et quasi, quidem natus, sint unde inventore!
+      </p>
+      <h1>Home</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe perferendis quis architecto suscipit ipsam, nostrum reiciendis veniam alias amet, sed aliquam a delectus provident odio incidunt quos dignissimos, dolorem aspernatur.
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit aspernatur corporis voluptatum. Animi ducimus earum error distinctio molestiae quisquam optio voluptatibus illo pariatur et quasi, quidem natus, sint unde inventore!
+      </p>
+
+    </div>
+  );
 };
 
 export default Home;
